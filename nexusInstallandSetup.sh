22eb14eb37c810603318bdaf3a0a37fc3eafec7e
Gospodin <jovanlm95@gmail.com>
2022-04-12 06:10:49 -0700

    Modified : Added root escalation to nexusInstallandSetup.sh

--- nexusInstallandSetup.sh
+++ nexusInstallandSetup.sh
@@ -4,26 +4,28 @@
 
 cd ~/Downloads
 
-sudo apt update 
+sudo su -
 
-sudo apt install openjdk-8-jdk -y 
+apt update 
+apt install openjdk-8-jdk -y 
 
 # Setup user
 useradd -M -d /opt/nexus -s /bin/bash -r nexus
-echo "nexus   ALL=(ALL)       NOPASSWD: ALL" > /etc/sudoers.d/nexus
+echo "nexus   ALL=(ALL)       NOPASSWD: ALL" | sudo tee -a /etc/sudoers.d/nexus
 
 # Nexus repo donwload and setup
 wget https://sonatype-download.global.ssl.fastly.net/repository/downloads-prod-group/3/nexus-3.29.2-02-unix.tar.gz # Change version if needed 
-sudo mkdir /opt/nexus
-sudo tar xzf nexus-3.29.2-02-unix.tar.gz -C /opt/nexus --strip-components=1
-sudo chown -R nexus: /opt/nexus
+mkdir /opt/nexus
+tar xzf nexus-3.29.2-02-unix.tar.gz -C /opt/nexus --strip-components=1
+chown -R nexus: /opt/nexus
 
 sed -i 's/2703/1024/g' /opt/nexus/bin/nexus.vmoptions
 sed -i 's/#run_as_user=""/run_as_user="nexus"/' /opt/nexus/bin/nexus.rc
 sed -i 's/\../\./g' /opt/nexus/bin/nexus.vmoptions
 
 # Create nexus.service 
-sudo cat > /etc/systemd/system/nexus.service << 'EOL'
+ 
+cat > /etc/systemd/system/nexus.service << 'EOL'
 [Unit]
 Description=nexus service
 After=network.target
